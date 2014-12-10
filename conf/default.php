cdc595b141b4f14778cb3e6fa6f702eed9ecec9e
sbbowman <sbbowman@users.noreply.github.com>
2014-12-10 13:35:33 -0500

    Initial commit of default.php.

---
+++ conf/default.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Default settings for the authsmartcard plugin
+ *
+ * @author Stephen Bowman <sbbowman@gmail.com>
+ */
+
+// Enable logging?
+//$conf['log_to_file']		= true;
+
+// If log_to_file is enabled, where to log?  Make sure apache/php can write to this file
+//$conf['logfile']		= "/full/path/to/logfile";
